5dbe4dacaeb57eba6fff84447387767515a60b85
aryansharma2204 <aryansde2204@gmail.com>
2022-11-18 14:08:55 +0530

    Create CBASICS8.C

---
+++ CBASICS8.C
@@ -0,0 +1,18 @@
+// Program to print factor of two numbers
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[]){
+    int a, b, i;
+    printf("Enter the First Number\n");
+    scanf("%d", &a);
+    printf("Enter the Second Number\n");
+    scanf("%d", &b);
+
+    for(i = 1; i < a; i++) {
+        if((a % i == 0) && (b % i == 0)) {
+            printf("Factor is %d\n", i);
+        }
+    }
+    return 0;
+}
