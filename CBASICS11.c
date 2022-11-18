1a218ac3acbeb42f787f590e5d8268840bdfd513
aryansharma2204 <aryansde2204@gmail.com>
2022-11-18 14:40:08 +0530

    Create CBASICS11.c

---
+++ CBASICS11.c
@@ -0,0 +1,21 @@
+// WAP to enter a number and print its perfect no. or not
+
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a, i, sum = 0;
+    printf("Enter a Number");
+    scanf("%d", &a);
+    for(i = 1; i < a; i++) {
+        if(a % i == 0) {
+            sum = sum + i;
+        }
+    }
+    if(sum == a) {
+        printf("Perfect Number");
+    }
+    else {
+        printf("Not a Perfect Number");
+    }
+    return 0;
+}
