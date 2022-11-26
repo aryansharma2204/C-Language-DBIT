83c43f1c6a8e5dfbbeeefb25a938dd9331e3a682
aryansharma2204 <aryansde2204@gmail.com>
2022-11-26 16:59:32 +0530

    Update CBASICS11.c

--- CBASICS11.c
+++ CBASICS11.c
@@ -1,10 +1,10 @@
-//WAP to enter a number and print its perfect no. or not
+//WAP to enter an integer and print its perfect no. or not
 
 #include <stdio.h>
 int main(int argc, char const *argv[])
 {
     int a, i, sum = 0;
-    
+
     printf("Enter a Number");
     scanf("%d", &a);
     for(i = 1; i < a; i++) {
